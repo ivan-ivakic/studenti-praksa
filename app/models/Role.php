3653832f7f1e7c6dbe2ababc2af3d930e12e0d27
arescec <arescec@yandex.com>
2016-07-06 14:42:15 +0200

    File structure, models added

---
+++ app/models/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Role extends Model
+{
+    public $id;
+    public $guid;
+    public $name;
+    public $description;
+
+    public $created_at;
+    public $updated_at;
+
+    public function initialize(){
+		$this->hasMany("guid", "RoleUser", "user_guid");
+	}
+}
---
+++ public/index.php
@@ -0,0 +1,57 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Config\Adapter\Ini as ConfigIni;
+
+try {
+
+    $config = new ConfigIni("../app/config.ini");
+
+    $loader = new Loader();
+    $loader->registerDirs(array(
+        $config->phalcon->controllersDir,
+        $config->phalcon->modelsDir,
+    ))->register();
+
+    $di = new FactoryDefault();
+
+    $di->set('view', function () use ($config) {
+        $view = new View();
+        $view->setViewsDir($config->phalcon->viewsDir);
+        return $view;
+    });
+
+    $di->set('url', function () use ($config) {
+        $url = new UrlProvider();
+        $url->setBaseUri($config->phalcon->baseUri);
+        return $url;
+    });
+
+    $di->set('db', function() use ($config){
+        try {
+            $db = new \Phalcon\Db\Adapter\Pdo\Mysql(
+                array(
+                    "host" => $config->database->host,
+                    "username" => $config->database->username,
+                    "password" => $config->database->password,
+                    "dbname" => $config->database->dbname
+                )
+            );
+        } catch (Exception $e) {
+            die("<b>Error initializing database connection:</b> " . $e->getMessage());
+        }
+        return $db;
+    });
+
+    $application = new Application($di);
+
+    echo $application->handle()->getContent();
+
+} catch (\Exception $e) {
+     echo "Exception: ", $e->getMessage();
+}
