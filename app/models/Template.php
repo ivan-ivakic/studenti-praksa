05f775443caa70a73a1813f5241bff20645709df
arescec <arescec@yandex.com>
2016-07-07 15:33:50 +0200

    Fixes as per pull request notes

---
+++ app/models/Template.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Template extends Model
+{
+    public $id;
+    public $guid;
+    public $current_version;
+
+    public $created_at;
+    public $updated_at;
+
+    public function initialize(){
+        $this->hasMany("guid", "TemplateDetails", "template_guid");
+        $this->hasMany("guid", "Alarm", "template_guid");
+    }
+}
