670c6cc776ed017c526e06f6f081197d3884f739
Moreno Muskovic <moreno.muskovic1@gmail.com>
2016-07-12 15:19:23 +0200

    Fixes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,23 +1,25 @@
 <?php
 
-use Phalcon\Paginator\Adapter\Model as PaginatorModel;
+use Phalcon\Paginator\Adapter\QueryBuilder as PaginatorModel;
 
 class UserController extends BaseController{   
 
     public function listAction(){
         // Current page to show
-        $currentPage = 1;
-        $currentPage = $this->request->getQuery('page', 'int');
+        $currentPage = $this->request->getQuery('page', 'int', 1);
 
-        $users = User::find(
-            array(
-                'order' =>  'created_at DESC'
-            )
-        );
+        
+        $builder = $this->modelsManager->createBuilder()
+                        ->from('User')
+                        ->join('RoleUser', 'User.guid = RoleUser.user_guid')
+                        ->join('Role', 'Role.guid = RoleUser.role_guid')
+                        ->distinct(true)
+                        ->orderBy('User.created_at');
+
 
         $paginator = new PaginatorModel(
             array(
-                "data"  => $users,
+                "builder"  => $builder,
                 "limit" => 5,
                 "page"  => $currentPage
             )
